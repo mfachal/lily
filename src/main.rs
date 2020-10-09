84a0cf90dc9b0b4035d504ab5c23047ce6b0e593
mfachal <647731+mfachal@users.noreply.github.com>
2020-10-09 01:09:28 -0300

    i literally have no idea what i did

--- src/main.rs
+++ src/main.rs
@@ -3,7 +3,11 @@ use std::net::{TcpListener, TcpStream};
 use std::fs;
 use std::io::{Write, Error, BufReader, BufRead};
 
-//use std::result::Result;
+
+mod test;
+
+
+
 
 fn getline(stream: &TcpStream) -> String {
 
@@ -42,68 +46,71 @@ impl fmt::Display for Entry {
 	}
 }
 
-static mut entries: Vec<Entry> = vec![];
-
-fn search_entries(who: String) -> Result<&'static Entry, &'static u8> {
-	unsafe {
+fn search_entries(who: String, entries: & Vec<Entry>) -> Result<& Entry, u8> {
+//	unsafe {
 		// TODO: if using threads mutex this shit
-		for entry in & entries {
+		for entry in entries {
 			let mut s = entry.selector.clone();
 			s.push_str("\r\n");
 			if who == s {
 				return Ok(entry);
 			}
 		}
-	}
-	return Err(&0);
+//	}
+	return Err(0);
 }
-/*
-fn send_list(stream: & TcpStream) {
-	// update_list()
-	let list = fs::read_to_string("gopher.db");
-	match list {
-		// TODO: comply with final .
-		Ok(s) => stream.write(s.as_bytes()),
-		Err(s) => stream.write("Error retrieving file list".as_bytes()),
-	};
-}
-*/
+
 fn answer(mut stream: &TcpStream, file: String){
 	// TODO: update_list();
 	let f = fs::read_to_string(file);
 	// TODO: security consideration: check file is in database
 	match f {
-		Ok(s) => stream.write(s.as_bytes()),
-		Err(_) => stream.write("Error retrieving file.".as_bytes()),
+		Ok(s) => stream.write(s.as_bytes()).expect("Couldn't write to socket.\n"),
+		Err(_) => stream.write("Error retrieving file.".as_bytes()).expect("Couldn't write to socket.\n"),
 	};
-	stream.write("\r\n.\r\n".as_bytes());
+	stream.write("\r\n.\r\n".as_bytes()).expect("Couldn't write to socket.\n");
 }
 
-fn handle_client(stream: &TcpStream){
+fn update_list(entries: & mut Vec<Entry>){
+
+	let entry = Entry {
+		item_type: "a".to_string(),
+		display_string: "a".to_string(),
+		selector: "a".to_string(),
+		hostname: "a".to_string(),
+		port: 0};
+
+	entries.push(entry);
+
+}
+
+fn handle_client(stream: &TcpStream, mut entries: & mut Vec<Entry>){
 	let received = getline(& stream);
 	println!("received: {}", received);
 
 	//later it will get populated each time an entry in the filesystem is updated
-	let entry = search_entries(received);
+	let entry = search_entries(received, entries);
 
 	match entry {
 		Ok(e) => answer(& stream, e.selector.clone()),
 		Err(_) => answer(& stream, "gopher.db".to_string()),
 	};
+
+	update_list(& mut entries);
+
 }
-
-
-
 
 fn main() -> Result<(), Error> {
 	let listener = TcpListener::bind("127.0.0.1:3000")?;
 	let port = listener.local_addr()?;
 	println!("Listening on {}", port);
 
+	let mut entries: Vec<Entry> = vec![];
+
 	for stream in listener.incoming() {
 		// let (mut tcp_stream, addr) = listener.accept()?; //block  until requested
 		println!("Connection received! sending data."); //, addr);
-		handle_client(&mut stream?);
+		handle_client(&mut stream?, &mut entries);
 	}
 	Ok(())
 }
